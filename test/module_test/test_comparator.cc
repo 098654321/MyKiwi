715755368cb7767c93f6c75cb85813d64b52b344
jiaheng_tang <25113040087@m.fudan.edu.cn>
2025-11-11 12:04:09 +0800

    add test for final version

--- test/module_test/test_comparator.cc
+++ test/module_test/test_comparator.cc
@@ -5,10 +5,10 @@ using namespace kiwi;
 
 
 static void test_comparint_two_mode_controlbits() {
-    // std::string file1{"..\\algorithm\\incremental_routing\\2025_10_30_test\\test_mode1_with_standard_file_of_mode2\\controlbits_comparison_in_cycles\\15\\4_controlbits_1.txt"};
-    std::string file1{"controlbits_1.txt"};
-    // std::string file2{"..\\algorithm\\incremental_routing\\2025_10_30_test\\test_mode1_with_standard_file_of_mode2\\controlbits_standard.txt"};
-    std::string file2{"controlbits_2.txt"};
+    std::string file1{"..\\algorithm\\incremental_routing\\2025.11.6_test\\test_2\\controlbits_standard_file.txt"};
+    // std::string file1{"controlbits_1.txt"};
+    std::string file2{"..\\algorithm\\incremental_routing\\2025.11.6_test\\test_2\\controlbits_load5_2.txt"};
+    // std::string file2{"controlbits_2.txt"};
     parse::compare(file1, file2);
 }
 
