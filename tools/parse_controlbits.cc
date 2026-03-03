ec08d64898a6730d039a4a5e4b020c867818b3c9
jiaheng_tang <25113040087@m.fudan.edu.cn>
2026-03-04 00:11:01 +0800

    updating parse_controlbits()

--- tools/parse_controlbits.cc
+++ tools/parse_controlbits.cc
@@ -1,56 +1,26 @@
-// load tob
-    // registers:
-    // tob2bump_bank/bump2tob_bank: value=0/1 for begin bump, value=1/0 for end bump; value=0/0 for bump not used
-    // dly, drv: useless
-    // hctrl_bank, vctrl_bank, bank_sel: parse mux value
-    // tob2track, track2tob: value=1/0 for begin track, value=0/1 for end track; value=0/0 for track not used
-
-    // datastructure:
-    // unordered_map1: begin_bump -> end_bump
-    // unordered_map2: bump -> tuple<tobconnector, track>
-    // unordered_map3: begin_bump -> end_track
-    // unordered_map4: begin_track -> end_bump
-
-    // algo:
-    // 1. find all the begin_bumps & end_bumps by tob2bump_bank/bump2tob_bank
-    // 2. find all the begin_tracks & end_tracks by tob2track/track2tob
-    // 3. parse mux_values using begin_bumps & end_bumps(load_tobconnector()), and get begin_tracks_parse & end_tracks_parse respectively
-    // 4. check if begin_tracks_parse & end_tracks_parse are consistent with begin_tracks & end_tracks respectively
-    // 5. for all the bumps, construct tobconnector, and construct unordered_map2
-    
-// load cob
-    // registers:
-    // <dir>_sel: value=0 for signal goes from track into cob, value=1 for signal goes from cob into track
-    // sw_<dir>: value=0 for disconnected, value=1 for connected
-
-    // datastructure:
-    // unordered_map5: begin_track -> path(track+cobconnector)
-
-    // algo:
-    // 1. for all the begin_tracks, find the path using algo in load_path_with_maze and store the path in unordered_map5
-
-// build nets using unordered_map1, mode, name, and add the net to basedie
-// set path for all the nets using unordered_map1, 2, 3
-
-#include <global/debug/debug.h>
-#include <global/utility/collection.h>
+#include <global/debug/debug.hh>
+#include <global/std/collection.hh>
 #include <hardware/interposer.hh>
-#include <parse/controlbits.hh>
+#include <parse/reader/controlbits/controlbits.hh>
 #include <string>
 #include <tuple>
 #include <unordered_map>
 #include <exception>
+#include <type_traits>
 
+
+namespace kiwi {
 
 auto print_help() -> void;
 auto parse_arguments(int, char**) -> std::tuple<std::string, int>;
-auto load_hardware(hardware::Interposer*, const parser::ControlBits&) -> std::tuple<std::set<hardware::Bump*>, std::set<hardware::Bump*>, std::set<hardware::Track*>, std::set<hardware::Track*>>;
-auto check_hardware(const std::set<hardware::Bump*>&, const std::set<hardware::Bump*>&, const std::set<hardware::Track*>&, const std::set<hardware::Track*>&) -> std::tuple<std::unordered_map<hardware::Bump*, hardware::TOBConnector>, std::unordered_map<hardware::Bump*, hardware::TobConnector>>;
+auto load_hardware(hardware::Interposer*, const parse::Controlbits&) -> std::tuple<std::set<hardware::Bump*>, std::set<hardware::Bump*>, std::set<hardware::Track*>, std::set<hardware::Track*>>;
+auto check_bump_and_track(hardware::Interposer*, const parse::Controlbits&,  std::set<hardware::Bump*>&, const std::set<hardware::Bump*>&, const std::set<hardware::Track*>&, const std::set<hardware::Track*>&) -> std::tuple<std::unordered_map<hardware::Bump*, hardware::TOBConnector>, std::unordered_map<hardware::Track*, hardware::TOBConnector>>;
 
 
 /////////////////////////////////////////////////////////////
 
 int main(int argc, char** argv) {
+try{
     if (argc != 3) {
         print_help();
     }
@@ -66,17 +36,34 @@ int main(int argc, char** argv) {
     auto basedie = std::make_unique<circuit::BaseDie>();
 
     // step4: load controlbits
-    auto controlbits = load_controlbits(folder, mode);
+    auto controlbits = parse::load_controlbits(folder, mode);
+    if (!controlbits.has_value()) {
+        throw std::logic_error("Controlbits not found");
+    }
 
     // step5: load hardware from tob controlbits
     // notice: only part of the tracks are loaded here, because some other tracks are IO ports of the interposer
     //         and will be loaded during the maze algorithm
-    auto [begin_bumps, end_bumps, begin_tracks, end_tracks] = load_hardware(interposer.get());
+    auto [begin_bumps, end_bumps, begin_tracks, end_tracks] = load_hardware(interposer.get(), controlbits.value());
 
     // step6: check if there is error in the loaded hardware
-    auto [begin_tobconnectors, end_tobconnectors] = check_hardware(begin_bumps, end_bumps, begin_tracks, end_tracks);
+    auto [begin_tobconnectors, end_tobconnectors] = check_bump_and_track(interposer.get(), controlbits.value(), begin_bumps, end_bumps, begin_tracks, end_tracks);
 
-    // 到这里有bump->tobconnector, track的信息
+    // step7: 根据 cob 的controlbits信息开始寻找路径
+    //  遍历所有的begin_tracks，对于每个begin_track，根据cob的controlbits信息做bfs的扩展搜索
+    //  搜索的时候，如果遇到一个end_track，就回溯得到一条路径。然后构建一条从begin_bump到end_bump的net，再把相应的begin/end_bump, begin/end_track从记录中删除，把net加入basedie
+    //  搜索的时候，如果遇到一个track是外部IO端口的，就构建一条从begin_bump到end_track的net，再把相应的begin_bump, begin_track从记录中删除
+
+    // step8: 检查是否还有从IO到bump的net
+    //  检查end_bumpds是否为空。如果是空的，说明都构造完了；如果不空，说明存在从IO为起点到end_bump的net
+    //  如果不空，那么遍历end_tracks。对于每一个end_track，利用cob controlbits信息倒着得到从IO到end_track的路径，然后构建对应的net、删除end_track, end_bump的记录，把net加入basedie
+
+    // step9: show net info
+}
+catch(const std::exception& e) {
+    debug::error(std::format("Unexpected exception: {}", e.what()));
+    return -1;
+}
 }
 
 /////////////////////////////////////////////////////////////
@@ -102,7 +89,7 @@ std::tuple<std::string, int> parse_arguments(int argc, char** argv) {
         return -1;
     };
 
-    auto show_help = [](std::string& info) -> void {
+    auto show_help = [](std::string info) -> void {
         debug::info(info);
         print_help();
         std::exit(-1);
@@ -126,10 +113,10 @@ std::tuple<std::string, int> parse_arguments(int argc, char** argv) {
     }
     auto mode = arguments[mode_index + 1];
 
-    return {file_path, mode};
+    return {file_path, std::stoi(mode)};
 }
     
-auto load_hardware(hardware::Interposer* interposer, const parser::ControlBits& controlbits) -> std::tuple<std::set<hardware::Bump*>, std::set<hardware::Bump*>, std::set<hardware::Track*>, std::set<hardware::Track*>>
+auto load_hardware(hardware::Interposer* interposer, const parse::Controlbits& controlbits) -> std::tuple<std::set<hardware::Bump*>, std::set<hardware::Bump*>, std::set<hardware::Track*>, std::set<hardware::Track*>>
 {
     auto begin_bumps = std::set<hardware::Bump*>{};
     auto end_bumps = std::set<hardware::Bump*>{};
@@ -139,33 +126,43 @@ auto load_hardware(hardware::Interposer* interposer, const parser::ControlBits& 
 
     for (auto& [tobcoord, bits]: controlbits.tob_bumpsig_direction) {
         for (std::size_t i = 0; i < N; i++) {
-            if (bits[i] == hardware::TOBBumpDirection::BumpToTrack) {
-                begin_bumps.insert(interposer->get_bump(tobcoord, i));
+            auto bump = interposer->get_bump(tobcoord, i);
+            if (!bump.has_value()) {
+                throw std::logic_error("load_hardware(): Bump not found");
+            }
+            if (bits[i] == hardware::TOBBumpDirection::BumpToTOB) {
+                begin_bumps.insert(*bump);
             } else {
-                end_bumps.insert(interposer->get_bump(tobcoord, i));
+                end_bumps.insert(*bump);
             }
         }
     }
     for (auto& [tobcoord, bits]: controlbits.tob_tracksig_direction) {
         for (std::size_t i = 0; i < N; i++) {
-            if (bits[i] == hardware::TOBTrackDirection::TrackToBump) {
-                begin_tracks.insert(interposer->get_track(tobcoord, i));
+            auto coord_in_interposer = hardware::Interposer::TOB_COORD_MAP.at(tobcoord);
+            auto track = interposer->get_track(coord_in_interposer.row, coord_in_interposer.col, hardware::TrackDirection::Vertical, i);
+            if (!track.has_value()) {
+                throw std::logic_error("load_hardware(): Track not found");
+            }
+            if (bits[i] == hardware::TOBTrackDirection::TrackToTOB) {
+                begin_tracks.insert(*track);
             } else {
-                end_tracks.insert(interposer->get_track(tobcoord, i));
+                end_tracks.insert(*track);
             }
         }
     }
 }
 
-auto check_hardware(const std::set<hardware::Bump*>& begin_bumps, const std::set<hardware::Bump*>& end_bumps, const std::set<hardware::Track*>& begin_tracks, const std::set<hardware::Track*>& end_tracks) -> std::tuple<std::unordered_map<hardware::Bump*, hardware::TOBConnector>, std::unordered_map<hardware::Bump*, hardware::TobConnector>>
+auto check_bump_and_track(hardware::Interposer* interposer, const parse::Controlbits& controlbits, const std::set<hardware::Bump*>& begin_bumps, const std::set<hardware::Bump*>& end_bumps, const std::set<hardware::Track*>& begin_tracks, const std::set<hardware::Track*>& end_tracks) -> std::tuple<std::unordered_map<hardware::Bump*, hardware::TOBConnector>, std::unordered_map<hardware::Track*, hardware::TOBConnector>>
 {
     auto begin_tracks_copy = begin_tracks;
     auto end_tracks_copy = end_tracks;
     auto begin_tobconnectors = std::unordered_map<hardware::Bump*, hardware::TOBConnector>{};
-    auto end_tobconnectors = std::unordered_map<hardware::Bump*, hardware::TOBConnector>{};
+    auto end_tobconnectors = std::unordered_map<hardware::Track*, hardware::TOBConnector>{};
 
-    auto get_tobconnector = [](hardware::Bump* bump, hardware::TOBBumpDirection dir, auto& bump_to_tobconnector) -> void {
+    auto get_tobconnector = [&controlbits, interposer](hardware::Bump* bump, hardware::TOBBumpDirection dir, auto& to_tobconnector) -> void {
         auto tobcoord = bump->tob()->coord();
+        auto coord_in_interposer = hardware::Interposer::TOB_COORD_MAP.at(tobcoord);
         auto bump_index = bump->index();
         
         auto hctrl = controlbits.bumptohctrl.at(tobcoord).at(bump_index) + (bump_index/8)*8;
@@ -173,34 +170,45 @@ auto check_hardware(const std::set<hardware::Bump*>& begin_bumps, const std::set
         auto vctrl = controlbits.hctrltovctrl.at(tobcoord).at(trans_hctrl) + (hctrl/64)*64 + (hctrl%8)*8;
         auto track_index = controlbits.vctrltotrack.at(tobcoord)[vctrl%64] == 0? vctrl : (vctrl+64)%128;
         auto connector = bump->tob()->bump_track_connectors_chain(bump_index, track_index, dir); 
+        auto track = interposer->get_track(coord_in_interposer.row, coord_in_interposer.col, hardware::TrackDirection::Vertical, track_index);
+        if (!track.has_value()) {
+            throw std::logic_error("check_hardware(): Track not found");
+        }
+        auto tob_sig_direction = dir == hardware::TOBBumpDirection::BumpToTOB? hardware::TOBSignalDirection::BumpToTrack : hardware::TOBSignalDirection::TrackToBump;
 
-        bump_to_tobconnector.emplace(bump, connector);
-        bump->set_connected_track(interposer->get_track(tobcoord, track_index));
+        using connector_map_t = std::remove_reference_t<decltype(to_tobconnector)>;
+        using key_t = typename connector_map_t::key_type;
+        if constexpr (std::is_same_v<key_t, hardware::Bump*>) {
+            to_tobconnector.emplace(bump, connector);
+        } else if constexpr (std::is_same_v<key_t, hardware::Track*>) {
+            to_tobconnector.emplace(*track, connector);
+        }
+        bump->set_connected_track(*track, tob_sig_direction);
         
         debug::debug_fmt("load_tobconnector(): calculated index = {}->{}->{}->{}", bump_index, hctrl, vctrl, track_index);
-    }
+    };
 
-    auto check_track_index = [](const std::set<hardware::Bump*>& bumps, std::set<hardware::Track*>& tracks_copy, hardware::TOBBumpDirection dir, auto& bump_to_tobconnector) {
+    auto check_track_index = [get_tobconnector](const std::set<hardware::Bump*>& bumps, std::set<hardware::Track*>& tracks_copy, hardware::TOBBumpDirection dir, auto& to_tobconnector) {
         for (auto bump: bumps) {
-            get_tobconnector(bump, dir, bump_to_tobconnector);
+            get_tobconnector(bump, dir, to_tobconnector);
             auto track = bump->connected_track();
             if (tracks_copy.contains(track)) {
                 tracks_copy.erase(track);
             }
             else {
-                throw std::logic_error(std::format("bumps {} contains track {} that is not in tracks", bump->coord(), track->coord()));
+                throw std::logic_error(std::format("check_bump_and_track(): bumps {} contains track {} that is not in tracks", bump->coord(), track->coord()));
             }
         }
         if (!tracks_copy.empty()) {
-            throw std::logic_error("tracks contains track that is not connected to the bumps");
+            throw std::logic_error("check_bump_and_track(): tracks contains track that is not connected to the bumps");
         }
-    }
+    };
     
-    check_track_index(begin_bumps, begin_tracks_copy, hardware::TOBBumpDirection::BumpToTrack, begin_tobconnectors);
-    check_track_index(end_bumps, end_tracks_copy, hardware::TOBBumpDirection::TrackToBump, end_tobconnectors);
+    check_track_index(begin_bumps, begin_tracks_copy, hardware::TOBBumpDirection::BumpToTOB, begin_tobconnectors);
+    check_track_index(end_bumps, end_tracks_copy, hardware::TOBBumpDirection::TOBToBump, end_tobconnectors);
 
     return {begin_tobconnectors, end_tobconnectors};
 }
 
 
-
+}
